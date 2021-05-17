73dc0c84989a1207a780f4e2c8b4ff13aadab9a2
orionTK <45930859+orionTK@users.noreply.github.com>
2021-05-17 22:37:59 +0700

    add paging

---
+++ lib/blog_vttkieu_web/helpers/paginatorHelper.ex
@@ -0,0 +1,51 @@
+defmodule BlogVttkieuWeb.Helpers.PaginatorHelper do
+  use Phoenix.HTML
+
+  def render(conn, data, class: class) do
+    first = prev_button(conn, data)
+    pages = page_buttons(conn, data)
+    last = next_button(conn, data)
+
+    content_tag(:ul, [first, pages, last], class: class)
+  end
+
+  defp prev_button(conn, data) do
+    page = data.current_page - 1
+    disabled = data.current_page == 1
+    params = build_params(conn, page)
+
+    content_tag(:li, disabled: disabled) do
+      link to: "?#{params}", rel: "prev" do
+        "<"
+      end
+    end
+  end
+
+  defp page_buttons(conn, data) do
+    for page <- 1..data.total_pages do
+      class = if data.current_page == page, do: "active"
+      disabled = data.current_page == page
+      params = build_params(conn, page)
+
+      content_tag(:li, class: class, disabled: disabled) do
+        link(page, to: "?#{params}")
+      end
+    end
+  end
+
+  defp next_button(conn, data) do
+    page = data.current_page + 1
+    disabled = data.current_page >= data.total_pages
+    params = build_params(conn, page)
+
+    content_tag(:li, disabled: disabled) do
+      link to: "?#{params}", rel: "next" do
+        ">"
+      end
+    end
+  end
+
+  defp build_params(conn, page) do
+    conn.query_params |> Map.put(:page, page) |> URI.encode_query()
+  end
+end
